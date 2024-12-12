f79006e1591e6891af1166f7141d56d9e4600810
Sajan <n01715615@humber.ca>
2024-12-12 00:46:45 -0500

    created product class

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //first commit
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+}
